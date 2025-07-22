28ca8d49a7b07fa2187587dcc3d936ed3afbc503
alan <langwdalan@gmail.com>
2025-07-22 11:24:50 +0800

    chore: add rust deps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+  // 设置默认日志级别
+  if env::var("RUST_LOG").is_err() {
+    unsafe {
+      env::set_var("RUST_LOG", "info");
+    }
+  }
+  env_logger::init();
+
+  let app = Router::new().route("/", get(root));
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3330").await.unwrap();
+  log::info!("Starting storkitty on port 3330");
+  axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+  "Hello, World!"
 }
